2f6ca2ef21f4786ddfe10f0a406f62d0dae5c6c4
andres <cuelloandres.js@gmail.com>
2017-11-16 12:25:28 -0300

    styles add publicate

--- target/work/gspcompile/gsp_fake_MeLi_layoutsmain_gsp.groovy
+++ target/work/gspcompile/gsp_fake_MeLi_layoutsmain_gsp.groovy
@@ -50,7 +50,7 @@ printHtmlPart(8)
 })
 invokeTag('form','g',33,['action':("search"),'class':("rowList")],2)
 printHtmlPart(9)
-if(true && (true)) {
+if(true && (false)) {
 printHtmlPart(10)
 }
 else {
@@ -68,7 +68,7 @@ protected void init() {
 	this.jspTags = JSP_TAGS
 }
 public static final String CONTENT_TYPE = 'text/html;charset=UTF-8'
-public static final long LAST_MODIFIED = 1510842342357L
+public static final long LAST_MODIFIED = 1510843334727L
 public static final String EXPRESSION_CODEC = 'html'
 public static final String STATIC_CODEC = 'none'
 public static final String OUT_CODEC = 'html'
